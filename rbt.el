e20efafedc99e69235fde7ee9fbd5f7c4742f604
jheyming <jheyming@cloudera.com>
2015-10-26 15:44:47 -0700

    Added popups to query which review you want to use.

--- rbt.el
+++ rbt.el
@@ -3,7 +3,10 @@
 ;; Author: Joe Heyming <joeheyming@gmail.com>
 ;; Version: 0.1
 ;; Keywords: reviewboard, rbt
+;; Package-Requires: ((popup "0.5.3"))
 ;;
+;;; Commentary:
+;; 
 ;; Useful commands:
 ;; rbt-status --> shows your current open reviewboard items
 ;;
@@ -13,15 +16,21 @@
 ;;
 ;; rbt-review-commit --> useful when your cursor is currently over a git commit hash id
 ;;
+;; rbt-review-with-selected --> useful when you don't remember the review id,
+;;   and want to be prompted which review to use.
+;;
 ;; rbt-review-close --> useful when your cursor is currently over a review id
 ;;
 ;; rbt-review-discard --> useful when your cursor is currently over a review id
 
-(provide 'rbt)
+
+;;; Code:
+
 (require 'json)
+(require 'popup)
 
 (defun rbt-custom-compile (cmd)
-  "Run a custom compile command in a custom buffer named *rbt*"
+  "Run a custom compile CMD in a custom buffer named *rbt*."
   (let ((mybuf (current-buffer)))
     (if (get-buffer "*rbt*")
         (kill-buffer "*rbt"))
@@ -37,28 +46,56 @@
 (defvar rbt-last-root)
 
 (defun vc-get-root ()
-  (interactive)
+  "Get the current root of your version control system."
   (condition-case nil
       (setq rbt-last-root (vc-call-backend 'Git 'root (file-name-directory (buffer-file-name))))
     (error rbt-last-root)))
 
 (defun rbt-status ()
-  "runs rbt status and puts the output in *rbt*"
+  "Run 'rbt status' and puts the output in *rbt*."
   (interactive)
   (rbt-custom-compile "rbt status")
   (other-window 1)
   )
 
 (defun rbt-current-review-id ()
-  "Gets the current review id under your cursor, if any"
+  "Gets the current review id under your cursor, if any."
   (if (string-match "\\([0-9]+\\)" (or (current-word) ""))
       (match-string 0 (current-word))))
 
-(defun rbt-review (&optional review-id commit-id)
-  "Runs rbt review. If no review id is supplied, we create a new review with the git HEAD.
-  You can override the review id and the commit id calling this function externally"
+(defvar rbt-popup-keymap
+  (let ((keymap (make-sparse-keymap)))
+    (set-keymap-parent keymap popup-menu-keymap)
+    (define-key keymap [return] 'popup-select )
+    (define-key keymap [tab] 'popup-select )
+    keymap)
+  )
+
+
+(defun rbt-select-review()
+  "Query with popup containing the current reviews for this user.
+Return the selected review or nil"
   (interactive)
-  (if (not review-id) 
+  (let*
+      ((reviews-json
+        (json-read-from-string (shell-command-to-string (format "rbt api-get /review-requests/ --from-user=$USER"))))
+       (reviews (mapcar '(lambda(x) (format "%s -- %s"
+                                            (assoc-default 'id x)
+                                            (assoc-default 'summary x)
+                                            ))
+                        (assoc-default 'review_requests reviews-json)))
+       (selected-review))
+    (setq selected-review (popup-menu* reviews :keymap rbt-popup-keymap))
+    (if selected-review (car (split-string selected-review " -- ")))
+    ))
+
+(defun rbt-review (&optional review-id commit-id)
+  "Run 'rbt review'.
+If no review id is supplied, we create a new review with the git HEAD.
+Optional argument REVIEW-ID A review id from reviewboard.
+Optional argument COMMIT-ID A git commit id."
+  (interactive)
+  (if (not review-id)
       (setq review-id (rbt-current-review-id)))
   (if (not commit-id) (setq commit-id "--parent=HEAD~1"))
 
@@ -68,7 +105,7 @@
 
         ;; Read any json info about this specific review request
         (review-json
-         (if review-id 
+         (if review-id
              (json-read-from-string (shell-command-to-string (format "rbt api-get /review-requests/%s/" review-id)) )
            nil))
 
@@ -86,22 +123,31 @@
     )
   )
 
-(defun rbt-review-commit (review-id)
-  "Tries to extract a commit from the current word you are looking at, then runs rbt-review
-  The user must supply a review id to go with the commit id."
-  (interactive (list (read-string "Review id: ")))
-  (rbt-review review-id (current-word)))
+(defun rbt-review-commit ()
+  "Run `rbt-review' with a commit from the current word you are looking at.
+The user will be asked which review to use with the commit."
+  (interactive)
+  (rbt-review (rbt-select-review) (current-word)))
+
+(defun rbt-review-with-selected()
+  "Ask the user which review to post the latest git changes to"
+  (interactive)
+  (rbt-review (rbt-select-review)))
 
 (defun rbt-review-close (&optional review-id)
-  "Close a review. Tries to get the review id from the current word."
+  "Close a review.  Try to get the REVIEW-ID from the current word."
   (interactive)
-  (if (not review-id) 
+  (if (not review-id)
       (setq review-id (rbt-current-review-id)))
   (rbt-custom-compile (format "rbt close --close-type submitted %s" review-id) ))
 
 (defun rbt-review-discard (&optional review-id)
-  "Discard a review. Tries to get the review id from the current word."
+  "Discard a review.  Try to get the REVIEW-ID from the current word."
   (interactive)
-  (if (not review-id) 
+  (if (not review-id)
       (setq review-id (rbt-current-review-id)))
   (rbt-custom-compile (format "rbt close --close-type discarded %s" review-id) ))
+
+(provide 'rbt)
+
+;;; rbt.el ends here
