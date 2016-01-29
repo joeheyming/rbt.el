865c619f200afe877c56a44046f706361b676d0e
jheyming <jheyming@cloudera.com>
2016-01-29 10:51:25 -0800

    Bump version.

--- rbt.el
+++ rbt.el
@@ -1,7 +1,7 @@
 ;;; rbt.el --- Integrate reviewboard with emacs.
 
 ;; Author: Joe Heyming <joeheyming@gmail.com>
-;; Version: 0.1
+;; Version: 0.2
 ;; Keywords: reviewboard, rbt
 ;; Package-Requires: ((popup "0.5.3") (magit "20160128.1201")
 ;;
